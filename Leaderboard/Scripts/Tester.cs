0b9013396b8786214290af5c2e3547489e58a094
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-01-03 05:56:50 +0400

    Initial commit

---
+++ Leaderboard/Scripts/Tester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace fdddfdfs.Leaderboard
+{
+    public class Tester : MonoBehaviour
+    {
+        [SerializeField] private LeaderboardController _leaderboardController;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                _leaderboardController.ChangeLeaderboardActive("Leaders");
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                _leaderboardController.UploadResult(Random.Range(0, 100));
+            }
+        }
+    }
+}
